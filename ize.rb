efbcf9a12d3355e90c42dbd776afc9ea47a99621
Dmitry Borisov <kobrikx@gmail.com>
2021-10-29 22:07:29 +0300

    Add ize install tests4

--- ize.rb
+++ ize.rb
@@ -1,10 +1,11 @@
 class Ize < Formula
-  version "v0.0.1"
-  sha256 "f4c74c60073e3df7347303f10eb2a04d3dcf0436620813e94388c2ed95fdd220"
-  url "https://github.com/hazelops/ize/releases/download/#{version}/macOS"
   name "Ize"
+  version "v0.0.1"
   desc "IZE is an opinionated infrastructure wrapper that allows to use multiple tools in one infra: terraform, serverless, waypoint"
   homepage "https://github.com/hazelops/ize"
+  stable do
+  sha256 "f4c74c60073e3df7347303f10eb2a04d3dcf0436620813e94388c2ed95fdd220"
+  url "https://github.com/hazelops/ize/releases/download/#{version}/macOS"
 
   def install
       mv macOS, ize
