e50585fbfcb976d7e368b1f2f1cc773a6937f6f1
Dmitry Borisov <kobrikx@gmail.com>
2021-10-29 23:25:25 +0300

    Add ize install tests9

--- ize.rb
+++ ize.rb
@@ -1,5 +1,4 @@
 class Ize < Formula
-  name "Ize"
   version "v0.0.1"
   desc "IZE is an opinionated infrastructure wrapper that allows to use multiple tools in one infra: terraform, serverless, waypoint"
   homepage "https://github.com/hazelops/ize"
